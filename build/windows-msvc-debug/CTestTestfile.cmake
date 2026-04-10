bd86a3aa69d1aeb7f38cc6ccca6e91bba5340f55
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2026-04-10 16:06:16 -0500

    Remove generated CMake artifacts from repository

--- build/windows-msvc-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/ode/lumalink-json
-# Build directory: C:/ode/lumalink-json/build/windows-msvc-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/arduinojson-build")
-subdirs("_deps/unity-build")
-subdirs("test")
